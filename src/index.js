593ba7460b2ea1897cd2f292cad2a6e6a074d2b8
Thiago Ferreira <thiagodnf@gmail.com>
2022-09-30 21:18:48 -0400

    some improvements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import Assembler from "./Assembler";
+import CompilerError from "./CompilerError";
+import Instruction from "./Instruction";
+import InstructionUtils from "./utils/InstructionUtils";
+import RegexUtils from "./utils/RegexUtils";
 
-export default {Assembler};
+export default {
+    Assembler,
+    CompilerError,
+    Instruction,
+    InstructionUtils,
+    RegexUtils
+};
